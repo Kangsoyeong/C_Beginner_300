902ab4499bd460042db58e27fdc77388d00483ec
Kang soyeong <korea020808@naver.com>
2022-02-08 23:32:43 +0900

    002

---
+++ 002_변수형 개념 배우기.c
@@ -0,0 +1,13 @@
+//변수 x에 1을 저장하고, 변수 y에 2를 저장하여, x와 y의 합을 z에 저장하는 프로그램을 작성하시오.
+
+#include <stdio.h>
+
+int main(void) {
+
+	int x = 1;
+	int y = 2;
+	int z = x + y;
+
+	printf("%d", z);
+	return 0;
+}
