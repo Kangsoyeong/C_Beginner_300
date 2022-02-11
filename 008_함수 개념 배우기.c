2dc66b8f5e5b940d146e2191c1a7a89c61c55233
Kang soyeong <korea020808@naver.com>
2022-02-12 00:13:54 +0900

    008_설명 수정 & 추가
    변수 -> 인수

--- 008_함수 개념 배우기.c
+++ 008_함수 개념 배우기.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-int sum(int number); //함수 선언. sum이라는 이름의 함수에 number라는 변수를 사용하겠다.
+int sum(int number); //함수 선언. sum이라는 이름의 함수에 number라는 인수를 사용하겠다.
 
 int main(void) {
 
@@ -23,3 +23,66 @@ int sum(int number) { //함수 구현
 
 	return j;
 }
+
+/*
+* 
+1. 함수의 종류
+-라이브러리 함수
+stdio.h파일 안에 정의되어있는 printf(), scanf()와 같은 함수이다.
+
+-사용자 정의 함수
+사용자가 원하는 대로 정의하는 함수이다. 보통 함수의 특성을 살려 이름을 짓는다.
+
+2. 함수의 사용법
+함수는 main()위에 선언해주어야 한다.
+
+-선언과 구현을 나누어서 코드 작성
+#include <stdio.h>
+
+int add(int a, int b); //선언
+
+int main()
+{
+	return 0;
+}
+
+int add(int a, int b) //구현
+{
+	return (a + b);
+}
+
+-선언과 동시에 구현
+#include <stdio.h>
+
+int add(int a, int b) //56~59 선언 + 구현
+{
+	return (a + b);
+}
+
+int main()
+{
+	return 0;
+}
+
+-함수의 호출
+#include <stdio.h>
+
+int add(int a, int b); //함수 선언
+
+int main()
+{
+	int num1 = 3;
+	int num2 = 4;
+	int result;
+
+	result = add(num1, num2); //1. line 83과 연결되는 함수 호출 & 인수 전달 / 3. line 84에서 값을 반환받아 result에 값 대입
+
+	result 0;
+}
+
+int add(int a, int b)
+{
+	return (a + b); //2. 계산 후 line 77에 반환
+}
+
+*/
