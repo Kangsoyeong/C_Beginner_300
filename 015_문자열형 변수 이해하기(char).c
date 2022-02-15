c586cba29cb80d5b7e1c39eec1f0745b3194f91d
Kang soyeong <korea020808@naver.com>
2022-02-15 21:20:13 +0900

    015

---
+++ 015_문자열형 변수 이해하기(char).c
@@ -0,0 +1,12 @@
+//문자열형 변수 str을 정의하고, "대한민국"을 저장하는 프로그램을 작성하시오.
+
+#include <stdio.h>
+int main(void) {
+
+	char str[] = "대한민국";
+	printf("%s", str);
+
+	return 0;
+}
+
+//문자열 배열 str을 선언하고 이름 자체가 주소값인 str을 통해 출력을 한다.
