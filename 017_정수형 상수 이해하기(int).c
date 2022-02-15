5c93fa641012f18e5f5bcc2344bcc6b991f58ea3
Kang soyeong <korea020808@naver.com>
2022-02-15 21:21:11 +0900

    017

---
+++ 017_정수형 상수 이해하기(int).c
@@ -0,0 +1,12 @@
+//정수형 상수 HUNDRED_THOUSAND를 정의하고 HUNDRED_THOUSAND를 100,000 대신에 사용하는 프로그램을 작성 하세요.
+
+#include <stdio.h>
+#define HUNDRED_THOUSAND 100000
+
+int main(void) {
+
+	printf("%d", HUNDRED_THOUSAND);
+
+	return 0;
+
+}
