dde7238262a9ba7b5df27ddfdc46717bdd0a8ffa
Kang soyeong <korea020808@naver.com>
2022-02-17 22:04:40 +0900

    022

---
+++ 022_부호 연산자 이해하기(+,-).c
@@ -0,0 +1,13 @@
+//변수 x,y를 정의하고, x의 값은 +4로, y의 값은 -2로 초기화한 후 x+y와 x-y의 결과를 출력하는 프로그램을 작성하세요.
+
+#include <stdio.h>
+int main(void) {
+
+	int x = 4;
+	int y = -2;
+
+	printf("%d\n", x+y);
+	printf("%d", x-y);
+
+	return 0;
+}
